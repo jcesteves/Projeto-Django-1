8c9fa89c7b7c72f9b17a2a50c580f7db918a5d32
Julio Cesar Esteves <jesteves.undefined@gmail.coms>
2022-03-22 09:47:22 -0300

    Adicionando mais funcionalidades ao projeto.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+admin.site.register(Category)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
---
+++ recipes/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'recipes'
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("recipes/category/<int:category_id>/",
+         views.category, name="category"),
+    path("recipes/<int:id>/", views.recipes, name="detail"),
+
+]
